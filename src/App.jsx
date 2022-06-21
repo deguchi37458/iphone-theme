b1c519deacd5d7832724d08dd759ad64176e1946
deguchi37458 <deguchi37458@gmail.com>
2022-06-21 17:49:37 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "./assets/css/reset.min.css";
+import "./assets/css/style.css";
+
+export const App = () => {
+  return (
+    <>
+      <div class="back">
+        
+      </div>
+    </>
+  );
+}; 

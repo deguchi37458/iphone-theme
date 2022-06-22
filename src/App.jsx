a5cd8014490daee835995d5ed16feff91b5a7e58
deguchi37458 <deguchi37458@gmail.com>
2022-06-22 15:39:52 +0900

    create Home component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Header } from "./components/Header";
 import { Nav } from "./components/Nav";
+import { Home } from "./components/Home";
 
 import "./assets/css/reset.min.css";
 import "./assets/css/style.css";
@@ -14,9 +15,7 @@ export const App = () => {
         <div class="back">
           <Header/>
             <Routes>
-              <Route/>
-              <Route/>
-              <Route/>
+              <Route exact path="/" element={<Home />} />
             </Routes>
           <Nav/>
         </div>
---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+import "../assets/css/components/home.css";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <div className="app">
+          <a href="/bio">
+          </a>
+        </div>
+        <div className="app">
+          <a href="/work">
+          </a>
+        </div>
+        <div className="app">
+          <a href="/contact">
+          </a>
+        </div>
+        <div className="app">
+          <a href="https://github.com/deguchi37458">
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}; 

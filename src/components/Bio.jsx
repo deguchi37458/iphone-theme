ac83a5b7c3f49ed33b58fca4baa1d364f6a7750d
deguchi37458 <deguchi37458@gmail.com>
2022-06-23 13:40:31 +0900

    update Bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -7,7 +7,10 @@ export const Bio = () => {
     <>
       <div className="bio">
         <div className="bio__container">
-
+          <h1>Biography</h1>
+          <p>NAME : Hiroki Deguchi</p>
+          <p>BIRTH : 1994/05/07</p>
+          <p>E-MAil : deguchi37458@gmail.com</p>
         </div>
       </div>
     </>
